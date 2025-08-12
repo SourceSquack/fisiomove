4938016759c8be2537be56946e7fa5802805cecc
ZetahDev <zetahdev@gmail.com>
2025-08-12 16:16:13 -0500

    feat: Add quick access component with HTML, CSS, and TypeScript implementation

---
+++ frontend/src/app/pages/dashboard/components/quick-access/quick-access.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-access',
+  templateUrl: './quick-access.component.html',
+  styleUrls: ['./quick-access.component.css'],
+})
+export class QuickAccessComponent {}
