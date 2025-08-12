93f6a3ca43487c8c8e351b224db12e77852ea675
ZetahDev <zetahdev@gmail.com>
2025-08-12 16:15:04 -0500

    feat: Add conditional rendering for navbar based on dashboard route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./components/footer/footer.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected title = 'Fisiomove';
+  isDashboardRoute = false;
+
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isDashboardRoute = event.url.includes('/dashboard');
+      });
+  }
 }
