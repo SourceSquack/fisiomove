d68102f56d794b31123e06c658e6fbbc9c039a90
ZetahDev <zetahdev@gmail.com>
2025-08-15 18:19:34 -0500

    refactor: update environment configuration for development and production

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,7 +1,18 @@
 export const environment = {
-  API_URL: "http://localhost:8000/api/v1/",
-  FOOTER_SOCIAL_FACEBOOK: "https://www.facebook.com/fisio.move.2025",
-  FOOTER_SOCIAL_INSTAGRAM: "https://instagram.com/fisiomove_rd",
-  FOOTER_SOCIAL_TIKTOK: "https://www.tiktok.com/@fisiomove5",
-  FOOTER_SOCIAL_WHATSAPP: "https://wa.me/573105055633",
+  production: false,
+  API_URL: 'http://localhost:8000/api/v1/',
+
+  // API Endpoints
+  API_ENDPOINTS: {
+    AUTH: {
+      CHANGE_PASSWORD: 'auth/password',
+
+    },
+  },
+
+  // Social Media Links
+  FOOTER_SOCIAL_FACEBOOK: 'https://facebook.com/fisiomove',
+  FOOTER_SOCIAL_INSTAGRAM: 'https://instagram.com/fisiomove_rd',
+  FOOTER_SOCIAL_TIKTOK: 'https://www.tiktok.com/@fisiomove5',
+  FOOTER_SOCIAL_WHATSAPP: 'https://wa.me/573105055633',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,17 @@
 export const environment = {
   production: true,
-  FOOTER_SOCIAL_FACEBOOK: 'https://www.facebook.com/fisio.move.2025',
+  API_URL: "https://your-production-api.com/api/v1/",
+
+  // API Endpoints
+  API_ENDPOINTS: {
+    AUTH: {
+      CHANGE_PASSWORD: 'auth/password',
+
+    },
+  },
+
+  // Social Media Links
+  FOOTER_SOCIAL_FACEBOOK: 'https://facebook.com/fisiomove',
   FOOTER_SOCIAL_INSTAGRAM: 'https://instagram.com/fisiomove_rd',
   FOOTER_SOCIAL_TIKTOK: 'https://www.tiktok.com/@fisiomove5',
   FOOTER_SOCIAL_WHATSAPP: 'https://wa.me/573105055633',
