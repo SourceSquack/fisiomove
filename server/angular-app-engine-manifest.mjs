ef6b6947a00bb4dd5ef7ee62f84c2181c84554be
ZetahDev <zetahdev@gmail.com>
2025-09-04 19:59:04 +0000

    Deploying to gh-pages from @ SourceSquack/fisiomove@6fac444f3d652e9abb5c07ad97aeebfc085a67bc 🚀

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/fisiomove',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/fisiomove/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 147706, hash: 'd0eca9a8e935275cf3cd45289b3ff6cd2a5e0a5c9bc1bec9282f2a9adeba7f37', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17255, hash: '854a931a6280275099dc1a5b7e2970d813631b9969176dbc5ebb78036c0f57c9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 147716, hash: '521ccacfaf8204d1253ef25a9c54ccea4d0722f8efe57772b1b35379bf9fd125', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17265, hash: '6a82fb9d17521fbd9cbdc36fc397ba10ff8e15973790a3fe71ff113ed2585c8a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-EIJSLQIK.css': {size: 775638, hash: 'vkHGfAjzQME', text: () => import('./assets-chunks/styles-EIJSLQIK_css.mjs').then(m => m.default)}
   },
 };
